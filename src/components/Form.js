33bafafb6a61090fbf6f1584f5d15f14ccd5efd8
Ary Davani <arydavani@gmail.com>
2017-07-27 17:40:23 -0400

    working

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
-  constructor(){
-    super()
+  constructor(props){
+    super(props)
+    console.log('my props', this.props);
   }
 
   render() {
     return (
       <div className="container text-center">
-        <form>
+        <form onSubmit={ this.props.handleFormSubmit }>
           <div className="form-group">
-            <input type="text" className="form-control" placeholder="What is your name, pilot?" value={ this.props.pilot } onChange={ this.handleNameChange }/>
+            <input type="text" className="form-control" placeholder="What is your name, pilot?" value={ this.props.pilot } onChange={ this.props.handleNameChange }/>
             <input type="submit" className="btn btn-primary" value="Submit" />
           </div>
         </form>
